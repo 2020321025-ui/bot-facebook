40b62c2b658c817348b15c0da388171982b31d9c
2020321025-ui <2020321025@unh.edu.pe>
2026-09-02 16:58:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return "🤖 Bot de Facebook funcionando correctamente."
+
+@app.route("/webhook")
+def webhook():
+    return "Webhook preparado."
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
